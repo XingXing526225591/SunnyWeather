dafae2f6ca13a3ab553666fcbbf18389c702df93
XingXing <526225591@qq.com>
2023-03-11 17:20:16 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/sunnyweather/ui/weather/WeatherActivity.kt
@@ -0,0 +1,10 @@
+package com.example.sunnyweather.ui.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WeatherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
