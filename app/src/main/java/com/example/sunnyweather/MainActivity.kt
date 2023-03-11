f29d4439f93bc66cf0ce27951370610eac0469f0
XingXing <526225591@qq.com>
2023-03-11 13:31:09 +0800

    实现搜索全球城市数据功能

--- app/src/main/java/com/example/sunnyweather/MainActivity.kt
+++ app/src/main/java/com/example/sunnyweather/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.sunnyweather
+ package com.example.sunnyweather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.core.widget.addTextChangedListener
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
